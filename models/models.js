198443f13584e9ba275058f73cb9156776f8e344
safchik <safchik9@gmail.com>
2023-03-28 13:36:36 +0100

    ticket-4 added tests, modyfied models and controllers files

--- models/models.js
+++ models/models.js
@@ -1,8 +1,18 @@
 const db = require("../db/connection");
 
-exports.fetchTopics = () =>{
-    
+exports.fetchTopics = () => {
     return db.query(`SELECT * FROM topics;`).then((result) => {
-       return result.rows;
+      return result.rows;
+    });
+  };
+  
+  exports.fetchArticleById = (articleId) => {
+    return db
+      .query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
+      .then((result) => {
+        if (result.rows.length === 0) {
+          return null; // article with provided ID not found
+        }
+        return result.rows[0]; // return article object
       });
-}
+  };
